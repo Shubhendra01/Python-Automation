c717e01def5b72031f7939768bd2d368c979a930
unknown <shubhhhh17@gmail.com>
2022-03-24 22:56:06 +0530

    Add Unittesting

--- UnitTesting.py
+++ UnitTesting.py
@@ -1,12 +1,21 @@
 import unittest
 
+def add(x, y):
+    return x+y
 
 class MyApp2(unittest.TestCase):
 
     def test_case3(self):
-        pass
-    def test_case4(self):
-        pass
+        a = 10
+        b = 22
+        result = add(a,b)
+        self.assertEqual(a+b, result)
 
-if __name__=="__main__":
+    def test_case4(self):
+        a = 12.5
+        b = 12.9
+        result = add(a, b)
+        self.assertEqual(a+b, result)
+
+if __name__ == "__main__":
     unittest.main()
