3465213681550db14585569c805dfa8718b34ee5
unknown <shubhhhh17@gmail.com>
2022-03-24 22:42:09 +0530

    Python Unittesting Barefoot

---
+++ UnitTesting.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class MyApp2(unittest.TestCase):
+
+    def test_case3(self):
+        pass
+    def test_case4(self):
+        pass
+
+if __name__=="__main__":
+    unittest.main()
